ef775625c1b35ad560ea72ddd3978f700a822e31
Dmitriy B <kupperfeet@gmail.com>
2022-08-31 13:43:54 +0300

    task4 modiff3

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,14 +1,16 @@
 ﻿Console.Write("Введите число: ");
 int luckyNumber = int.Parse(Console.ReadLine() ?? "0");
 int count = 0;
+int i = 0;
 while (luckyNumber > 0)
 {
-    if(luckyNumber%10 == 4 | luckyNumber%10 == 7)
+    if(luckyNumber % 10 == 4 | luckyNumber % 10 == 7)
     {
         count++;
     }
 luckyNumber=luckyNumber/10;
+i++;
 }
-if (count == 4 | count == 7) Console.Write("YES");
+if (count == 4 | count == 7 | count == i) Console.Write("YES");
 
 else ("NO");
