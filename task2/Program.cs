61b94164a9340f1b4d21909e117cc3f342d5ad18
Dmitriy B <kupperfeet@gmail.com>
2022-08-31 11:44:31 +0300

    task2 done

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите расстояние до друга: ");
+int s = int.Parse(Console.ReadLine() ?? "0");
+int steps = 0;
+if (s % 5 > 0)
+{
+    steps = (s / 5) + 1;
+}
+else
+{
+    steps = s / 5;
+}
+Console.WriteLine($"{steps}");
