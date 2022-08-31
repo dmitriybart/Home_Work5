358c363325b62ae8fb62ebbb0e9f68264175b350
Dmitriy B <kupperfeet@gmail.com>
2022-08-31 11:19:20 +0300

    task1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите ширину площади: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите длину площади: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите размер плитки: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int x = 0;
+int y = 0;
+ if (n%a == 0) 
+  {
+    x = n / a;
+  }
+  else 
+  {
+    x = n / a;
+    x++;
+  }
+  if (m%a == 0) 
+  {
+    y = m / a;
+  }
+  else {
+    y = m / a;
+    y++;
+  }
+Console.Write($"{x*y}");
