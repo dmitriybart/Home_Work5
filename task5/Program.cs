762b607e8d305880e81902694f341a09e482054e
Dmitriy B <kupperfeet@gmail.com>
2022-08-31 14:06:34 +0300

    task5 modiff3

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите стоимость 1-го банана: ");
+int k = int.Parse(Console.ReadLine() ?? "00");
+
+Console.Write("Введите количество денег: ");
+int n = int.Parse(Console.ReadLine() ?? "00");
+
+Console.Write("Введите необходимое количество бананов: ");
+int w = int.Parse(Console.ReadLine() ?? "00");
+int i = 1;
+int sum = 0;
+
+while (i <= w)
+{ 
+    sum = sum + (i * k);
+    i++;
+}
+int credit = sum - n;
+if (credit<=0) Console.WriteLine($"Необходимо занять {credit = 0}");
+
+else Console.WriteLine($"Необходимо занять {credit}");
